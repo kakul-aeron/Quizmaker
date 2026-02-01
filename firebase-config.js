37b302f7dafc2cb8eec10caf6e4cfc4cc8cff944
kakulaeron <kakulaeron1106@gmail.com>
2026-02-01 15:40:46 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+// This file loads configuration from environment variables for security
+// Make sure to add .env to .gitignore to keep credentials safe
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmy1ZN5BdJfb-ppPNtAjvZ-kgJbRXraeg",
+    authDomain: "quizmaker-e296a.firebaseapp.com",
+    databaseURL: "https://quizmaker-e296a-default-rtdb.firebaseio.com",
+    projectId: "quizmaker-e296a",
+    storageBucket: "quizmaker-e296a.firebasestorage.app",
+    messagingSenderId: "656680380425",
+    appId: "1:656680380425:web:485fc654fb40848c2930d8",
+    measurementId: "G-HQZZ9226H8"
+};
